1f1abef110a0237cab30006a20dec9c72e7480ec
Jake <github@jakewendt.com>
2011-11-03 11:15:35 -0700

    Removed error message manipulation.

--- lib/common_lib/active_record_extension.rb
+++ lib/common_lib/active_record_extension.rb
@@ -3,4 +3,5 @@ require 'active_record'
 module CommonLib::ActiveRecordExtension; end
 require 'common_lib/active_record_extension/base'
 require 'common_lib/active_record_extension/errors'
-require 'common_lib/active_record_extension/error'
+#	No longer used
+#require 'common_lib/active_record_extension/error'
--- lib/common_lib/active_record_extension/error.rb
+++ lib/common_lib/active_record_extension/error.rb
@@ -1,18 +1,21 @@
 module CommonLib::ActiveRecordExtension::Error
-
-	def self.included(base)
-		base.alias_method_chain( :generate_full_message, :attribute_strip ) unless
-			base.method_defined?(:generate_full_message_without_attribute_strip)
-	end
-
-	def generate_full_message_with_attribute_strip(options = {}, &block)
-		m = generate_full_message_without_attribute_strip(options, &block)
-		unless( ( i = m.index('<|X|') ).nil? )
-			m = m[i+4..-1]
-		end
-		m
-	end
-	
+#
+#	def self.included(base)
+#		base.alias_method_chain( :generate_full_message, :attribute_strip ) unless
+#			base.method_defined?(:generate_full_message_without_attribute_strip)
+#	end
+#
+#	def generate_full_message_with_attribute_strip(options = {}, &block)
+#		m = generate_full_message_without_attribute_strip(options, &block)
+#		unless( ( i = m.index('<|X|') ).nil? )
+#			m = m[i+4..-1]
+#		end
+#		m
+#	end
+#	
 end	#	module CommonLib::ActiveRecordExtension::Error
-ActiveRecord::Error.send(:include,
-	CommonLib::ActiveRecordExtension::Error)
+#ActiveRecord::Error.send(:include,
+#	CommonLib::ActiveRecordExtension::Error)
+
+#	We do not use this anymore.  I wrote custom validations that
+#	effectively do the same thing and much cleaner.
