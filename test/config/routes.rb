9854080fec28a29157bfc55717f8affbbd7a5210
Jake <github@jakewendt.com>
2011-11-18 11:54:53 -0800

    Version bump to 0.1.0

--- test/config/routes.rb
+++
@@ -1,9 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-	map.resources :blogs,
-			:shallow => true do |blog|	
-		blog.resources :posts
-	end
-
-	map.root :controller => :blogs, :action => :index
-
-end
