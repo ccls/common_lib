42db4acf073e8192635cf190b286e2762bb058a9
Jake <github@jakewendt.com>
2011-12-02 23:32:36 -0800

    Added comment and alias

--- lib/common_lib/active_record_extension/errors.rb
+++ lib/common_lib/active_record_extension/errors.rb
@@ -4,11 +4,15 @@ module CommonLib::ActiveRecordExtension::Errors
 		base.send(:include,InstanceMethods)
 	end
 	module InstanceMethods
+		#
+		#	Can't believe this doesn't exist in rails.  It has proved very beneficial.
+		#
 		def on_attr_and_type(attribute,type)
 			attribute = attribute.to_s
 			return nil unless @errors.has_key?(attribute)
 			@errors[attribute].collect(&:type).include?(type)
 		end
+		alias_method :on_attr_and_type?, :on_attr_and_type
 	end
 	module ClassMethods
 		def delete(key)
